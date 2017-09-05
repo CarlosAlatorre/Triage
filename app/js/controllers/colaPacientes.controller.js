15769abd1d17b1e5adacf9ecf969f7feaabd48d0
CarlosAlatorre <carloS_alatorrE_95@hotmail.com>
2017-09-05 15:48:31 -0700

    - Se añadio la interfaz de datos basicos del modal "Registrar sintomas"

--- app/js/controllers/colaPacientes.controller.js
+++ app/js/controllers/colaPacientes.controller.js
@@ -7,28 +7,67 @@ triage
 
       //public var
       var vm = this;
+      vm.view = 'informacionBasica';
 
 
       //public functions
       vm.openRegisterPatient = openRegisterPatient;
+      vm.openRegisterSymptom = openRegisterSymptom;
+      vm.registerPatient = registerPatient;
+      vm.closeModalOfRegisterPatient = closeModalOfRegisterPatient;
+      vm.closeModalofRegisterSymptom = closeModalofRegisterSymptom;
+      vm.nextInModalOfRegisterSymptom = nextInModalOfRegisterSymptom;
 
 
       //private functions
       function activate() {
-        $scope.time =15;
-        chronoService.addTimer('myTimer', { interval: 500 });
-        chronoService.start();
+
+
       }
+
       activate();
 
-      function openRegisterPatient () {
-        vm.modal = $uibModal.open({
+      function openRegisterPatient() {
+        vm.modalRegisterPatient = $uibModal.open({
           animation: true,
           templateUrl: 'views/modals/registrarPaciente.modal.html',
           scope: $scope,
           size: 'lm',
           backdrop: 'static'
         });
+      }
+
+      function openRegisterSymptom() {
+        vm.modalRegisterSymptom = $uibModal.open({
+          animation: true,
+          templateUrl: 'views/modals/registrarSintomas.modal.html',
+          scope: $scope,
+          size: 'lm',
+          backdrop: 'static'
+        });
+      }
+
+      function registerPatient(patient) {
+
+        vm.modalRegisterPatient.dismiss();
+      }
+
+      function closeModalOfRegisterPatient() {
+
+        vm.modalRegisterPatient.dismiss();
+
+      }
+
+      function closeModalofRegisterSymptom() {
+
+
+        vm.modalRegisterSymptom.dismiss();
+      }
+
+      function nextInModalOfRegisterSymptom() {
+
+
+        vm.modalRegisterSymptom.dismiss();
       }
 
 
